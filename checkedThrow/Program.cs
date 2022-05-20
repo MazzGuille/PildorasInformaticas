386207cfd96ad7ffc476f86577fbc3ef7a6b649d
MazzGuille <mazzcode@gmail.com>
2022-05-20 15:21:04 -0300

    checked

---
+++ checkedThrow/Program.cs
@@ -0,0 +1,21 @@
+﻿checked
+{
+    
+int numero = int.MaxValue;
+ 
+int resultado = numero + 20;
+ 
+Console.WriteLine(resultado);
+
+}
+
+
+int numero2 = int.MaxValue;
+ 
+int resultado2 = numero2 + 20;
+ 
+Console.WriteLine(resultado2);
+
+
+
+
